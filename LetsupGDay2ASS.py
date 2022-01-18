f913fd08ea33b728070d8b4e6e0ec9cdb2d13b00
RSangaradoss <90277107+RSangaradoss@users.noreply.github.com>
2022-01-18 23:36:50 +0530

    Day 2 assignment submitted

---
+++ LetsupGDay2ASS.py
@@ -0,0 +1,58 @@
+#int
+a= 10 
+print(a)
+print(type(a))
+print(id(a))
+a = 20
+print(a)
+print(id(a))
+print(type(a))
+print()
+
+#float
+x = 15.56 
+print(type(x)) 
+print(int(x))
+print()
+
+#string
+x = "15"
+print(type(x)) 
+print(int(x))
+
+x = "15.56 "
+print(type(x)) 
+
+x = "Hello World"
+print(type(x)) 
+print()
+
+
+#bool
+b=True
+print(b)
+print(type(b))
+print()
+
+#list
+l=["apple", "banana", "cherry"]
+print(l)
+print(type(l)) 
+print()
+
+#tuple
+x=("apple", "banana", "cherry")
+print(x)
+print(type(x)) 
+print()
+
+
+
+#dict
+x = {"name" : "John", "age" : 36}
+print(x)
+print(type(x)) 
+print()
+
+
+
